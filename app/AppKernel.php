c4b2fa412855db50d02ff2f195266a371e6bba51
Florian <florob95@gmail.com>
2018-02-19 22:59:48 +0100

    add NelmioCorsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             //FosRest + JWT experiment
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
