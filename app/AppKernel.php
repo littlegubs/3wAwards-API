e8642bd5198a44b7981fb2c14e6e488c6a001067
Florian <florob95@gmail.com>
2018-02-19 22:59:30 +0100

    add FOSRestBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            //FosRest + JWT experiment
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
