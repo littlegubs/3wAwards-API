6d32a46a62f82db245ca909a10482c38361b2b77
lbourgeois <lucasbourgeois60@hotmail.fr>
2018-03-10 14:58:23 +0100

    TagFixtures methods

--- src/AppBundle/DataFixtures/ORM/TagFixtures.php
+++ src/AppBundle/DataFixtures/ORM/TagFixtures.php
@@ -9,24 +9,42 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
 class TagFixtures extends Fixture implements OrderedFixtureInterface
 {
-    private $tagsSiteType = ['Showcase', 'Mobile', 'WebApp', 'e-shop', 'Blog', 'Social', 'Visit card', 'Portfolio', 'Portal', 'Mag', 'Event', 'Intranet'];
-    private $tagsPurpose = ['Complete reshaping', 'Technical reshaping', 'Graphical reshaping', 'Version upgrade', 'Technology changement'];
-    private $tagsBusinessSector = ['Auto', 'Insurrance', 'Banking', 'Administration', 'Industry', 'Education'];
-    private $tagsTarget = ['B2B', 'B2C', 'C2B', 'C2C'];
-    private $tagsLanguage = ['fr', 'en', 'hr', 'ch', 'es', 'de', 'pl', 'ru', 'ja', 'nl', 'ro', 'wa', 'hi', 'ko', 'sv', 'vi', 'no'];
-    private $tagsBudgetFork = ['-5k', '5-10k', '10-20k', '20-40k', '40-80k', '80-120k', '120-160k', '160-200k', '200-240k', '240-280k', '280-320k', '320-360k', '360-500k', '500-750k', '750-900k', '900-1000k', '1000k+'];
-    private $tagsColor = ['Red', 'Yellow', 'Green', 'Blue', 'White' , 'Black', 'Grey', 'Light blue', 'Orange', 'Dark green', 'Light red', 'Gold', 'Purple', 'Pink', 'Brown', 'Salmon', ''];
-    private $tagsStyle = ['Flat design', 'OnePage', 'Flash', 'LongScroll', 'Parallax', 'Animations', 'Fullscreen', 'Video', 'Modern'];
-    private $tagsBehavior = ['Adaptative Design', 'Fluid Design', 'Infinite Scroll', 'Responsive'];
-    private $tagsAccessibility = [1, 1, 2, 2, 1, 1, 3, 1, 3, 1, 3, 2, 2, 2];
-    private $tagsAgencyMission = ['Concil', 'Front development', 'Back development', 'Ux Design', 'UI design', 'SEO', 'Visual Identity'];
-    private $tagsCustom = ['Voitures', 'Distributeur automobile', 'Concession', 'Occasion', 'Garage', 'Gueudet', 'Renault', 'BMW', 'MINI', 'Dacia', 'Toyota', 'Nissan', 'Opel'];
-    private $tagsMainFonctionnality = ['Catalog', 'Appointement manager', 'Multi brand', 'Advanced browser', 'Interactive map', 'Job', 'Import schedules', 'Automtic schedule updates', 'Intefacing'];
-    private $tagsFrontTech = ['HTML5', 'CSS3', 'Js', 'Ts', 'Angular', 'Twig', 'Jquery', 'Google Maps', 'Node', 'ReactJs'];
-    private $tagsBackTech = ['Php', 'Symfony', 'CakePhp', 'Elastic Search', 'MySql', 'MongoDb', 'NoSql', 'OpenSource', 'Kibana', 'Npm'];
-    private $tagsCms = ['Drupal', 'Wordpress', 'Prestashop', 'Shopify', 'Joomla', 'Magento', 'SmartBase (owner)'];
-    private $tagsChallenge = [1,2,5,5,4,2,1,5,2,4,3,5,1,4,4,1,1];
-
+    private $allTags = [
+        /* site type */
+        ['Showcase', 'Mobile', 'WebApp', 'e-shop', 'Blog', 'Social', 'Visit card', 'Portfolio', 'Portal', 'Mag', 'Event', 'Intranet'],
+        /* Purpose */
+        ['Complete reshaping', 'Technical reshaping', 'Graphical reshaping', 'Version upgrade', 'Technology changement'],
+        /* Business Sector */
+        ['Auto', 'Insurrance', 'Banking', 'Administration', 'Industry', 'Education'],
+        /* Target */
+        ['B2B', 'B2C', 'C2B', 'C2C'],
+        /* Language */
+        ['fr', 'en', 'hr', 'ch', 'es', 'de', 'pl', 'ru', 'ja', 'nl', 'ro', 'wa', 'hi', 'ko', 'sv', 'vi', 'no'],
+        /* Budget Fork */
+        ['-5k', '5-10k', '10-20k', '20-40k', '40-80k', '80-120k', '120-160k', '160-200k', '200-240k', '240-280k', '280-320k', '320-360k', '360-500k', '500-750k', '750-900k', '900-1000k', '1000k+'],
+        /* Colors */
+        ['Red', 'Yellow', 'Green', 'Blue', 'White', 'Black', 'Grey', 'Light blue', 'Orange', 'Dark green', 'Light red', 'Gold', 'Purple', 'Pink', 'Brown', 'Salmon', ''],
+        /* Style */
+        ['Flat design', 'OnePage', 'Flash', 'LongScroll', 'Parallax', 'Animations', 'Fullscreen', 'Video', 'Modern'],
+        /* Behavior */
+        ['Adaptative Design', 'Fluid Design', 'Infinite Scroll', 'Responsive'],
+        /* Accessibility */
+        [1, 1, 2, 2, 1, 1, 3, 1, 3, 1, 3, 2, 2, 2],
+        /* Agency missions */
+        ['Concil', 'Front development', 'Back development', 'Ux Design', 'UI design', 'SEO', 'Visual Identity'],
+        /* Custom */
+        ['Voitures', 'Distributeur automobile', 'Concession', 'Occasion', 'Garage', 'Gueudet', 'Renault', 'BMW', 'MINI', 'Dacia', 'Toyota', 'Nissan', 'Opel'],
+        /* Main Fonctionnality */
+        ['Catalog', 'Appointement manager', 'Multi brand', 'Advanced browser', 'Interactive map', 'Job', 'Import schedules', 'Automtic schedule updates', 'Intefacing'],
+        /* Front Tech */
+        ['HTML5', 'CSS3', 'Js', 'Ts', 'Angular', 'Twig', 'Jquery', 'Google Maps', 'Node', 'ReactJs'],
+        /* Back Tech */
+        ['Php', 'Symfony', 'CakePhp', 'Elastic Search', 'MySql', 'MongoDb', 'NoSql', 'OpenSource', 'Kibana', 'Npm'],
+        /* CMS */
+        ['Drupal', 'Wordpress', 'Prestashop', 'Shopify', 'Joomla', 'Magento', 'SmartBase (owner)'],
+        /* Challenge */
+        [1, 2, 5, 5, 4, 2, 1, 5, 2, 4, 3, 5, 1, 4, 4, 1, 1]
+    ];
 
     /**
      * @return int
@@ -41,14 +59,21 @@ class TagFixtures extends Fixture implements OrderedFixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-
+        foreach ($this->allTags as $typeTagKey => $tags){
+            foreach ($tags as $key => $tag){
+                $this->createTag($manager, $tag, $typeTagKey,$key);
+            };
+        };
     }
 
     /**
      * @param ObjectManager $manager
      */
-    private function createTag(ObjectManager $manager)
+    private function createTag(ObjectManager $manager, $tag, $typeTagKey, $key)
     {
-        $tag = new Tag();
+        $tagToCreate = new Tag();
+        $tagToCreate
+            ->setLibelle($tag)
+            ->setType($typeTagKey);
     }
 }
