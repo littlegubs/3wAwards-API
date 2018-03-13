1c3dcfb3fe80339850c582b84ca69aafce41a9d1
lbourgeois <lucasbourgeois60@hotmail.fr>
2018-03-13 22:25:25 +0100

    AwardFixture

--- src/AppBundle/DataFixtures/ORM/AwardFixtures.php
+++ src/AppBundle/DataFixtures/ORM/AwardFixtures.php
@@ -3,6 +3,8 @@
 namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Entity\Award;
+use AppBundle\Entity\Category;
+use AppBundle\Entity\Project;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
@@ -10,12 +12,9 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 class AwardFixtures extends Fixture implements OrderedFixtureInterface
 {
     private $awards = [
-        'Award de la semaine',
-        'Award du mois',
-        'Award de l\'année',
-        '',
-        '',
-        ''
+        Award::TYPE_DAY,
+        Award::TYPE_MONTH,
+        Award::TYPE_YEAR
     ];
 
     /**
@@ -31,14 +30,29 @@ class AwardFixtures extends Fixture implements OrderedFixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-
+        foreach ($this->awards as $key => $value) {
+            $this->createAward($manager, $key, $value);
+        }
+        $manager->flush();
     }
 
     /**
      * @param ObjectManager $manager
      */
-    private function createAward(ObjectManager $manager)
+    private function createAward(ObjectManager $manager, $i, $type)
     {
+        /** @var Category $category */
+        $category = $this->getReference('category_'.rand(0, 6));
+//        /** @var Project $project */
+//        $project = $this->getReference('project'.rand(0, 6));
+
         $award = new Award();
+        $award->setDate(new \DateTime(rand(1, 28).'-'.rand(1, 12).'-'.rand(2012, 2018)));
+        $award->setCategory($category);
+        $award->setType($type);
+//        $award->setProject($project);
+
+        $manager->persist($category);
+        $this->addReference('award_'.$i, $award);
     }
 }
