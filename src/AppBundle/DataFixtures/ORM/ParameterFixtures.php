4cc8a3eef3505d2ab0312950a9e97d589840e956
Decourty William <williamdecourty@live.fr>
2018-03-13 22:45:54 +0100

    Ajout Parameter datafixtures

--- src/AppBundle/DataFixtures/ORM/ParameterFixtures.php
+++ src/AppBundle/DataFixtures/ORM/ParameterFixtures.php
@@ -9,14 +9,39 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
 class ParameterFixtures extends Fixture implements OrderedFixtureInterface
 {
-    private $parameters = [
-        '',
-        '',
-        '',
-        '',
-        '',
-        ''
-    ];
+    private $libelles = [
+      'app_name',
+      'logo',
+      'adress',
+      'complement',
+      'tel',
+      'fax',
+      'email',
+      'email_admin_alert',
+      'email_admin_ask',
+      'med_ranking_judges',
+      'med_ranking_members',
+      'nb_max_screen',
+      'size_max_screen',
+      'nb_max_tags'
+    ]
+
+    private $values = [
+      '3Wawards',
+      'jacky.jpg',
+      '60204 COMPIEGNE CEDEX',
+      'CS70454 LACROIX SAINT-OUEN';
+      '0344862255',
+      '0344862277',
+      'contact@mentalworks.fr',
+      'contact@mentalworks.fr',
+      'contact@mentalworks.fr',
+      '3',
+      '4',
+      '6',
+      '10',
+      '5'
+    ]
 
     /**
      * @return int
@@ -31,14 +56,21 @@ class ParameterFixtures extends Fixture implements OrderedFixtureInterface
      */
     public function load(ObjectManager $manager)
     {
-
+      foreach ($this->libelles as $key => $value) {
+          $this->createParameter($manager, $key);
+      }
+      $manager->flush();
     }
 
     /**
      * @param ObjectManager $manager
      */
-    private function createParameter(ObjectManager $manager)
+    private function createParameter(ObjectManager $manager, $i)
     {
-        $parameter = new Parameter();
+      $parameter = new Parameter();
+      $parameter
+        ->setLibelle($this->$libelles[$i])
+        ->setValue($this->$values[$i]);
+      $manager->persist($parameter);
     }
 }
