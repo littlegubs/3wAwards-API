89c9d3985e1c041117ccd5042c199c924958685f
florob95 <florob95@gmail.com>
2018-05-21 18:03:21 +0200

    command for assignment award day

--- src/AppBundle/Command/AssignmentAwardDayCommand.php
+++ src/AppBundle/Command/AssignmentAwardDayCommand.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Command;
 
 use AppBundle\Entity\Award;
+use AppBundle\Entity\Project;
+use AppBundle\Entity\ProjectRatingMember;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputInterface;
@@ -39,19 +41,38 @@ class AssignmentAwardDayCommand extends Command
     {
         $date = new \DateTime();
         $now = $date->format('Y-m-d');
+        $bestRatingProject = 0;
 
         $output->writeln('<info>Start assignement award for the day '.$now.'</info>');
 
-       $AllprojectRatingMember = $this->entityManager->getRepository('AppBundle:ProjectRatingMember')->findAll();
-
-
+        $allProjectRatingMember = $this->entityManager->getRepository('AppBundle:ProjectRatingMember')->findAll();
+       foreach ($allProjectRatingMember as $projectRatingMember) {
+           $ratingProject = $projectRatingMember->getProject()->getAverageRating() /
+               $this->countVoteByProjects($allProjectRatingMember, $projectRatingMember->getProject()->getId());
+           if ($ratingProject > $bestRatingProject) {
+               $bestRatingProject = $ratingProject;
+               $bestProject = $projectRatingMember->getProject();
+           }
+       }
        $newAward = new Award();
        $newAward->setDate($date);
+       $newAward->setProject($bestProject);
        $newAward->setType('day');
 
        $this->entityManager->persist($newAward);
        $this->entityManager->flush();
 
+        $output->writeln('<info>'.$bestProject->getProjectName().' wins the award for this day</info>');
+    }
+
+    function countVoteByProjects($allProjectratingMember, $idProject) {
+        $nbVotes = 0;
+        foreach ($allProjectratingMember as $projectRatingMember) {
+            if ($projectRatingMember->getProject()->getId() === $idProject) {
+                $nbVotes = $nbVotes + 1;
+            }
+        }
+        return $nbVotes;
     }
 
 
