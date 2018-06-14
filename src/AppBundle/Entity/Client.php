2cecaa412ab5913b9be905553893d906498e8df4
florob95 <florob95@gmail.com>
2018-06-14 09:54:27 +0200

    api property

--- src/AppBundle/Entity/Client.php
+++ src/AppBundle/Entity/Client.php
@@ -7,7 +7,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Serializer\Annotation\Groups;
-use ApiPlatform\Core\Annotation\ApiProperty;
 
 /**
  * Client
